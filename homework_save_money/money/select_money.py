2dbd00e278494575de65390e0f20a88403c82dc5
greg <799734522.qq.com>
2020-12-07 23:33:33 +0800

    homework1

---
+++ homework_save_money/money/select_money.py
@@ -0,0 +1,7 @@
+# 查询工资
+import money
+
+
+def select_money():
+    saved_money = money.saved_money
+    print('最新存款:' + str(saved_money))
---
+++ homework_save_money/money/send_money.py
@@ -0,0 +1,8 @@
+# 发工资
+import money
+
+
+def send_money():
+    print('今天发工资')
+
+    money.saved_money = money.saved_money + 1000
